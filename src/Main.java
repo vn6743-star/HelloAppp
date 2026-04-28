e4406fa9ea64ab341a565ee1c5e08171cdc08f56
vn6743-star <vn6743@srmist.edu.in>
2026-04-28 14:59:35 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+        String names = "World";
+
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
-
-        StringBuilder nameBuilder = new StringBuilder();
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
         System.out.println("Hello, " + names + "!");
     }
